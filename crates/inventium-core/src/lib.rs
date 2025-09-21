b55c4982c21fc0a7c6a8272631cde71e919b1574
oganesson897 <oganesson897@gmail.com>
2025-09-22 08:42:22 +0900

    chore

--- crates/inventium-core/src/lib.rs
+++ crates/inventium-core/src/lib.rs
@@ -1,7 +1,41 @@
-use isahc::{Body, HttpClient, HttpClientBuilder, Response};
+use isahc::{Body, HttpClient, HttpClientBuilder, Response, Request as IsaRequest};
+use isahc::http::request::Builder;
 
-pub trait Request {
-    fn get(&self, url: &str) -> Response<Body> {
-        isahc::get(url).unwrap()
+pub struct UrlBuilder {
+    pub url: String
+}
+
+impl UrlBuilder {
+    pub fn new(base_url: &str) -> Self {
+        Self {
+            url: base_url.to_string() + "?"
+        }
+    }
+
+    pub fn add_param(mut self, key: &str, value: &str) -> Self {
+        self.url.push_str(&format!("{}={}&", key, value));
+        self
+    }
+}
+
+pub struct Request {
+    pub client: HttpClient
+}
+
+impl Request {
+    pub fn new(agent: &str) -> Self {
+        let mut builder = isahc::HttpClient::builder();
+
+        builder = builder.default_header("Content-Type", "application/json");
+        builder = builder.default_header("Accept", "application/json");
+        builder = builder.default_header("User-Agent", agent);
+
+        Self {
+            client: builder.build().unwrap()
+        }
+    }
+
+    pub fn get(&self, url: &str) -> Builder {
+        IsaRequest::get(url)
     }
 }
